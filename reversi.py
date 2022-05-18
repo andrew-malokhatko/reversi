f3533123f776c705fed96174fa1c9caacb650da8
andrew-malokhatko <94763793+andrew-malokhatko@users.noreply.github.com>
2022-05-18 20:09:53 +0300

    code refactoring

--- reversi.py
+++ reversi.py
@@ -12,12 +12,12 @@ DESKCOLOR = (40, 255, 80)
 
 screen = pygame.display.set_mode(SCREENSIZE) #variables
 game_on = True
-t = 0
+t = 0 # is for turns
 
-x_offset = (SCREENSIZE.x - BLOCKSIZE * 8)/2
-y_offset = (SCREENSIZE.y - BLOCKSIZE * 8)/2  # offsets
+x_offset = (SCREENSIZE.x - BLOCKSIZE * 8) / 2
+y_offset = (SCREENSIZE.y - BLOCKSIZE * 8) / 2
 
-class Tile(pygame.sprite.Sprite): # tile (i know, cool comment)
+class Tile(pygame.sprite.Sprite):
     def __init__(self, x, y, size):
         super().__init__()
         self.pos = Position(x, y)
@@ -27,24 +27,24 @@ class Tile(pygame.sprite.Sprite): # tile (i know, cool comment)
         self.image.fill(self.color)
         self.rect = self.image.get_rect(topleft=(self.pos))
 
-    def update(self, screen):
+    def update(self, screen): 
         self.draw(screen)
 
-    def on_click(self):
+    def on_click(self): # changing color of clicked tile
         global t
         x, y = pygame.mouse.get_pos()
         if self.rect.collidepoint(x, y) and self.color == DESKCOLOR:
             self.color = PLAYERCOLOR if t % 2 == 1 else BOTCOLOR
             self.image.fill(self.color)
             t += 1
-            return Position(self.pos.x, self.pos.y)
+            return self.pos
 
     def draw(self, screen: pygame.Surface):
         self.image.fill(self.color)
         screen.blit(self.image, self.rect)
-        pygame.draw.rect(screen, (20, 140, 20), self.rect, 2, 0, 0, 0, 0, 0) # change borders color here!
+        pygame.draw.rect(screen, (20, 140, 20), self.rect, 2, 0, 0, 0, 0, 0) # drawing borders change borders color here!
 
-class Desk(): # list of tiles also its doing all operations with them
+class Desk: # list of tiles also its doing all operations with them
     def __init__(self): # doing SIDE x SIDE desk filled with tiles
         self.tiles = []
         for i in range(SIDE):
@@ -52,15 +52,14 @@ class Desk(): # list of tiles also its doing all operations with them
                 tile = Tile(x_offset + BLOCKSIZE * i, y_offset + BLOCKSIZE * k, BLOCKSIZE)
                 self.tiles.append(tile)
 
-    def update_tiles(self, screen : pygame.Surface): # updates tiles
+    def update_tiles(self, screen : pygame.Surface):
         for tile in self.tiles:
             tile.update(screen)
 
     def render_desk(self, x, y): # checking and changing tiles color when necessary
+
         def in_range(ind): # checking is index in range of list
-            if ind < 0 or ind >= SIDE*SIDE:
-                return False
-            return True
+            return ind >= 0 and ind < SIDE * SIDE
 
         def to_index(x, y): # gets index by position
             for tile in self.tiles:
@@ -71,38 +70,31 @@ class Desk(): # list of tiles also its doing all operations with them
         def move(ind, color, step): # going through one line of blocks
             i = step
             on_axis = []
-            while True:
-                if in_range(ind + i):
-                    if self.tiles[ind + i].color == DESKCOLOR: 
-                        break
-                    elif self.tiles[ind + i].color == color: 
-                        if len(on_axis) != 0:
-                            for tile in on_axis:
-                                tile.color = color
-                        break
-                    else:
-                        on_axis.append(self.tiles[ind + i])
-                        i += step
-                else:
+            while in_range(ind + i):
+                if self.tiles[ind + i].color == DESKCOLOR: 
                     break
+                elif self.tiles[ind + i].color == color: 
+                    for tile in on_axis:
+                        tile.color = color
+                    break
+                else:
+                    on_axis.append(self.tiles[ind + i])
+                    i += step
 
         ind = to_index(x, y)
         print(ind)
 
-        while True:
-            if ind != None:
-                color = self.tiles[ind].color
-                move(ind, color, 1) # down
-                move(ind, color, -1) # up
-                move(ind, color, -8) # left
-                move(ind, color, 8) # right
-                move(ind, color, -9) # left top
-                move(ind, color, 7) # right top
-                move(ind, color, 9) # right down
-                move(ind, color, -7) # left down
-            break
-                
-
+        if ind != None:
+            color = self.tiles[ind].color
+            move(ind, color, 1) # down
+            move(ind, color, -1) # up
+            move(ind, color, -8) # left
+            move(ind, color, 8) # right
+            move(ind, color, -9) # left top
+            move(ind, color, 7) # right top
+            move(ind, color, 9) # right down
+            move(ind, color, -7) # left down
+            
 desk = Desk()
         
 while game_on:
@@ -110,10 +102,10 @@ while game_on:
         if event.type == pygame.QUIT:
             game_on = False
 
-        if event.type == pygame.MOUSEBUTTONDOWN:
+        elif event.type == pygame.MOUSEBUTTONDOWN:
             for tile in desk.tiles:
                 pos = tile.on_click()
-                if pos != None:
+                if pos:
                     desk.render_desk(pos.x, pos.y)
 
     screen.fill((0, 0, 0))
